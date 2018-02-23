a2458938a873c4e8573de0da412c429d824d77b8
sakakun <matthew.bate@sk-projects.net>
2018-02-23 09:30:30 -0500

    Step IF Step 3

--- StudentProfile/Program.cs
+++ StudentProfile/Program.cs
@@ -22,7 +22,7 @@ namespace StudentProfile
         static void Main(string[] args)
         {
             const int miniumYearsToGraduation = 1;                              // interger for miniumYearsToGraduation
-            const int maximumYearsToGraduation = 1;                             // interger for maximumYearsToGraduation
+            const int maximumYearsToGraduation = 4;                             // interger for maximumYearsToGraduation
 
             string userInput = string.Empty;                                    // string for user input
 
@@ -32,15 +32,43 @@ namespace StudentProfile
 
             Console.Write("Please enter your name: ");                          // Prompt the user for their name
             userInput = Console.ReadLine();                                     // Read user input
-
             if (userInput.Trim().Length > 0)
-            { studentName = userInput; }
-            else { Console.Write("Error: What you entered {0} is not a name. Please try again", userInput); }
+            {
+                studentName = userInput;
+                Console.Write("Please enter your program name: ");                  // Prompt the user for their name
+                userInput = Console.ReadLine();                                     // Read user input
+                if (userInput.Trim().Length > 0)
+                {
+                    programName = userInput;
+                    Console.Write("Please enter the number of years to completion for your program: ");                  // Prompt the user for their name
+                    userInput = Console.ReadLine();
+                    if (int.TryParse(userInput, out yearsToCompletion) &&
+                        ((yearsToCompletion >= miniumYearsToGraduation) &&
+                        (yearsToCompletion <= maximumYearsToGraduation)))
+                    {
+                        // Success
+                        Console.WriteLine("Years to Completion: " + yearsToCompletion);
+                    }
+                    else
+                    {
+                        Console.Write("Error: What you entered {0} is not a number. Expecting a number between {1:n0} and {2:n0}. Please try again", userInput, miniumYearsToGraduation, maximumYearsToGraduation);
+                    }
+                }
+                else
+                {
+                    Console.Write("Error: What you entered {0} is not a name. Please try again", userInput);
+                }
+            }
+            else {
+                Console.Write("Error: What you entered {0} is not a name. Please try again", userInput);
+            }
 
-
+ 
+            /*
             Console.WriteLine("Name: " + studentName);                          // Output Student Name
             Console.WriteLine("Program: " + programName);                       // Output Program Name
             Console.WriteLine("Years to Completion: " + yearsToCompletion.ToString());     // Output Years to completion
+            */
             Console.Write("\nPress Any Key to Exit...");                        // Display Any Key Message
             Console.ReadKey();                                                  // Hold for Any Key
         } //Program
